7a2f31ac752ca98b723ee904143d8777f01a0b94
Elfi_Y <elfiyang16@gmail.com>
2021-08-03 12:08:13 +0100

    feat: add another method

--- examples/python/helloworld/greeter_client.py
+++ examples/python/helloworld/greeter_client.py
@@ -26,9 +26,15 @@ def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = helloworld_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+    # with grpc.insecure_channel('localhost:50051') as channel:
+    #     stub = helloworld_pb2_grpc.GreeterStub(channel)
+    #     response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+    # print("Greeter client received: " + response.message)
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = helloworld_pb2_grpc.GreeterStub(channel)
+    response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+    print("Greeter client received: " + response.message)
+    response = stub.SayHelloAgain(helloworld_pb2.HelloRequest(name='you'))
     print("Greeter client received: " + response.message)
 
 
--- examples/python/helloworld/greeter_server.py
+++ examples/python/helloworld/greeter_server.py
@@ -26,8 +26,9 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
 
     def SayHello(self, request, context):
         return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
-
-
+    
+    def SayHelloAgain(self, request, context):
+        return helloworld_pb2.HelloReply(message='Hello again, %s!' % request.name)
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
